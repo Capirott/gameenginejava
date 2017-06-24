debf446acf033aabf252176584ca81141da628ac
Erickson <erickson.lucas@live.com>
2017-06-24 18:54:09 -0300

    End of tutorial 6. Textures added.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,11 +2,13 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TextureModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -27,15 +29,22 @@ public class MainGameLoop {
 			3, 1, 2
 		};
 		
+		float[] textureCoors = {
+			0, 0,
+			0, 1,
+			1, 1,
+			1, 0
+		};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
-		
+		RawModel model = loader.loadToVAO(vertices, textureCoors, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TextureModel textureModel = new TextureModel(model, texture);
 
 		
 		while (!Display.isCloseRequested()) {
 			shader.start();
 			renderer.prepare();
-			renderer.render(model);
+			renderer.render(textureModel);
 			shader.start();
 			DisplayManager.updateDisplay();
 		}
