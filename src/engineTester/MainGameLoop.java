caf68fb1c42313d3e470973633a1b582d62082f6
Erickson <erickson.lucas@live.com>
2017-07-08 21:14:32 -0300

    End of tutorial 20. Mipmapping.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -36,16 +36,20 @@ public class MainGameLoop {
 		ModelData data2 = OBJFileLoader.loadOBJ("fern");
 		ModelData data3 = OBJFileLoader.loadOBJ("grass");
 		ModelData data4 = OBJFileLoader.loadOBJ("dragon");
+		ModelData data5 = OBJFileLoader.loadOBJ("box");
+		
 		RawModel model = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), data.getNormals(), data.getIndices());
 		RawModel fern = loader.loadToVAO(data2.getVertices(), data2.getTextureCoords(), data2.getNormals(), data2.getIndices());
 		RawModel grass = loader.loadToVAO(data3.getVertices(), data3.getTextureCoords(), data3.getNormals(), data3.getIndices());
 		RawModel bunny = loader.loadToVAO(data4.getVertices(), data4.getTextureCoords(), data4.getNormals(), data4.getIndices()); 
+		RawModel box = loader.loadToVAO(data5.getVertices(), data5.getTextureCoords(), data5.getNormals(), data5.getIndices());
 		
 		TexturedModel playerModel = new TexturedModel(bunny, new ModelTexture(loader.loadTexture("white")));
 		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("tree")));
 		TexturedModel fernModel = new TexturedModel(fern, new ModelTexture(loader.loadTexture("fern")));
 		TexturedModel grassModel = new TexturedModel(grass, new ModelTexture(loader.loadTexture("grass2")));
-		
+		TexturedModel boxModel = new TexturedModel(box, new ModelTexture(loader.loadTexture("box")));
+
 		fernModel.getTexture().setHasTransparency(true);
 		fernModel.getTexture().setUseFakeLighting(true);
 		grassModel.getTexture().setHasTransparency(true);
@@ -64,9 +68,11 @@ public class MainGameLoop {
 					new Vector3f(random.nextFloat() * 1600, 0, random.nextFloat() * 1600), 0, 0, 0, 1));
 			entities.add(new Entity(grassModel,
 					new Vector3f(random.nextFloat() * 1400 + 100, 0, random.nextFloat() * 1400 + 100), 0, 0, 0, 3));
+			entities.add(new Entity(boxModel,
+					new Vector3f(random.nextFloat() * 3600 + 100, 3, random.nextFloat() * 3600 + 100), 0, 0, 0, 3));
 		}
 
-		Light light = new Light(new Vector3f(20000, 20000, 2000), new Vector3f(1, 1, 1));
+		Light light = new Light(new Vector3f(20000, 20000, 2000), new Vector3f(0.99f, 0.83f, 0.25f));
 
 		
 		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy"));
