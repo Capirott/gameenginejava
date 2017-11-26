bac39ddfee565855f4fa9e941a904c8ff4ddd055
Erickson Silva <erickson.lucas@live.com>
2017-11-26 19:15:50 -0200

    ...

--- src/entities/Joint.java
+++ src/entities/Joint.java
@@ -3,9 +3,11 @@ package entities;
 import org.lwjgl.util.vector.Vector3f;
 
 import models.TexturedModel;
+import toolbox.Maths;
 
 public class Joint extends Entity {
 
+	private float rotYP;
 	private Vector3f offset = new Vector3f(0.0f, 0.0f, 0.0f);
 	public Joint(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
@@ -19,7 +21,12 @@ public class Joint extends Entity {
 	
 	@Override
 	public Vector3f getPosition() {
-		return Vector3f.add(super.getPosition(), offset, new Vector3f());
+		return Maths.rotateYWithAnchor(Vector3f.add(super.getPosition(), offset, new Vector3f()), super.getPosition(), rotYP);
 	}
-	
+	public float getRotYP() {
+		return rotYP;
+	}
+	public void setRotYP(float rotYP) {
+		this.rotYP = rotYP;
+	}
 }
